f753a95e3c7718a5e1f8bbcff059073d10f77d60
Tinei <tinei@hotmail.fr>
2016-07-29 21:22:28 +0200

    Utilisation de la classe Rectangle dans le fichier principal

--- main.cpp
+++ main.cpp
@@ -4,11 +4,9 @@ using namespace std;
 int main()
 {
   cout << "Bienvenue." << endl;
-  int longueur = 6;
-  int largeur = 3;
-  int aire = longueur*largeur;
-  cout << "Soit un rectangle de longueur " << longueur << " et de largeur " << largeur << "." << endl;
-  cout << "Aire du rectangle: " << aire << endl;
+  Rectangle rect(6, 3);
+  cout << "Soit un rectangle de longueur " << rect.getLongueur() << " et de largeur " << rect.getLargeur() << "." << endl;
+  cout << "Aire du rectangle: " << rect.aire() << endl;
   
   return 0;
 }
