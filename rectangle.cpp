46613ee0d0e695ef86444d51b960dbeaebfb8d58
Tinei <tinei@hotmail.fr>
2016-07-29 21:20:19 +0200

    Ajout d'un constructeur et d'accesseurs pour la classe Rectangle (définitions)

--- rectangle.cpp
+++ rectangle.cpp
@@ -1,5 +1,18 @@
 #include "rectangle.h"
 
+Rectangle::Rectangle(int longueur, int largeur) : longueur(longueur), largeur(largeur)
+{}
+
+int getLongueur() const
+{
+  return longueur; 
+}
+
+int getLargeur() const
+{
+  return largeur;
+}
+
 void Rectangle::aire() const
 {
   return longueur*largeur;
