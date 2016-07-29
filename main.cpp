3e36dec3b67fd9ed85fb218954d5ca9ce0b4a5bd
Tinei <tinei@hotmail.fr>
2016-07-29 19:49:36 +0200

    Ajout de la fonctionnalité "rectangle"
    Permet de calculer l'aire d'un rectangle

--- main.cpp
+++ main.cpp
@@ -4,5 +4,11 @@ using namespace std;
 int main()
 {
   cout << "Bienvenue." << endl;
+  int longueur = 6;
+  int largeur = 3;
+  int aire = longueur*largeur;
+  cout << "Soit un rectangle de longueur " << longueur << " et de largeur " << largeur << "." << endl;
+  cout << "Aire du rectangle: " << aire << endl;
+  
   return 0;
 }
